a40bc9d8c1ae5c718345aa6d693b368085b63ba6
Aaron Hanusa <aaron.hanusa@gmail.com>
2017-06-09 20:25:15 -0500

    add ValidCityRule

---
+++ BusinessLogic/Rules/ValidCityRule.cs
@@ -0,0 +1,19 @@
+using Peasy;
+
+public class ValidCityRule : RuleBase
+{
+    private string _city;
+
+    public ValidCityRule(string city)
+    {
+        _city = city;
+    }
+
+    protected override void OnValidate()
+    {
+        if (_city == "Nowhere")
+        {
+            Invalidate("Nowhere is not a city");
+        }
+    }
+}
